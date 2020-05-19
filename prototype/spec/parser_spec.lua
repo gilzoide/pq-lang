01d5c580b3184c9d79e86a84087c7779620a26b8
gilzoide <gilzoide@gmail.com>
2020-05-18 22:43:05 -0300

    Add some more tests

--- prototype/spec/parser_spec.lua
+++ prototype/spec/parser_spec.lua
@@ -1,15 +1,55 @@
 local Parser = require 'parser'
 
-describe("When parsing line,", function()
+describe("When parsing input,", function()
     local parser = Parser.new()
 
-    it("returns list of tokens", function()
-        local line = "1 2 3   +4'\tfive"
-        local res = parser:parse(line)
+    it("returns list of tokens in line", function()
+        local text = "1 2 3   +4'\tfive"
+        local res = parser:parse(text)
         assert.are.same({'1', '2', '3', '+4\'', 'five'}, res)
     end)
 
-    describe("errors with unbalanced parenthesis", function()
+    it("returns one list per non-empty line", function()
+        local text = [[
+        one
+        per line
+
+        skips empty lines
+        ]]
+        local res = parser:parse(text)
+        assert.are.same({
+            {'one'},
+            {'per', 'line'},
+            {'skips', 'empty', 'lines'},
+        }, res)
+    end)
+
+    it("parenthesized atoms become sublist #NYI", function()
+        local text = "outside (inside)  (another inside one)"
+        local res = parser:parse(text)
+        assert.are.same({'outside', {'inside'}, {'another', 'inside', 'one'}}, res)
+    end)
+
+    it("parenthesis across multiple lines become a single list #NYI", function()
+        local text = [[
+        (this is 
+            a single
+            list
+        )
+        (
+            likewise
+        )
+        back to normal
+        ]]
+        local res = parser:parse(text)
+        assert.are.same({
+            {'this', 'is', 'a', 'single', 'list'},
+            {'likewise'},
+            {'back', 'to', 'normal'},
+        }, res)
+    end)
+
+    describe("errors with unbalanced parenthesis #NYI", function()
         local lines = {
             "(",
             ")",
@@ -17,9 +57,9 @@ describe("When parsing line,", function()
             "())",
         }
 
-        for i, line in ipairs(lines) do
+        for i, text in ipairs(lines) do
             it(string.format("-- %q", line), function()
-                assert.has.errors(function() parser:parse(line) end)
+                assert.has.errors(function() parser:parse(text) end)
             end)
         end
     end)
